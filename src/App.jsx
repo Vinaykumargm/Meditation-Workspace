a4bd409e9f8ccacad0635caed33cb10daded15ea
Vinay kumar G M <162595523+Vinaykumargm@users.noreply.github.com>
2025-01-25 22:40:03 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Home from "./pages/Home";
+import Home from "./Pages/Home"
 import MeditationPage from "./pages/MeditationPage";
 import ResourcesPage from "./pages/ResourcesPage";
 import StaticExperiences from "./pages/StaticExperiences";
